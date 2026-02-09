caaa01b2b5911258b73ed5bae695660ac14c9e1f
blskk <blskkat7174@gmail.com>
2026-02-09 15:02:09 +0200

    first commit

---
+++ src/main/java/com/tm/TripBudgeter/TripBudgeterApplication.java
@@ -0,0 +1,13 @@
+package com.tm.TripBudgeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripBudgeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripBudgeterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tm/TripBudgeter/TripBudgeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tm.TripBudgeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripBudgeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
